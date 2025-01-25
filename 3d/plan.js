f1d71f4b69a911d8f177a4c7e2fac2fb695dbc46
Beeka <beekayt274@gmail.com>
2025-01-26 00:19:02 +1300

    second pass: objects and shit

--- 3d/plan.js
+++ 3d/plan.js
@@ -1,11 +1,44 @@
 var plan = {
     grid: `
-        #####
-        #####
-        ##S##
-        #####
-        #####
+    ##3##
+    #3#3#
+    3###3
+    ###$#
+    ..#..
+    ..#..
+    ..#..
+    #####
+    ##3##
+    ###s#
+    ##S##
+    #####
     `.replace(/\s/g, '').split(''),
     width: 5,
-    height: 5,
+    height: 12,
+    entities: {
+        'S': {
+            id: 'player',
+            exec: (i)=>{
+                player.x = i % plan.width
+                player.y = Math.floor(i / plan.width)
+            }
+        },
+        '3': {
+            class: 'colonthree',
+            tileclass: 'dontbullshit'
+        },
+        's': {
+            class: 'sign spacemono',
+            tileclass: 'dontbullshit nowalk',
+            content: '<b>exhibit a</b><p>freaking <a href="https://corru.observer">observe</a> this shit</p>'
+        },
+        '$': {
+            class: 'sign spacemono',
+            tileclass: 'dontbullshit nowalk',
+            content: '<b>exhibit a*</b><p>holy fucking <b>bingle</b></p>'
+        },
+        '.': {
+            tileclass: 'empty nowalk'
+        }
+    }
 }
--- 3d/script.js
+++ 3d/script.js
@@ -1,22 +1,22 @@
-var player = {x: 0, y: 0, rotate: 0}
+const player = {x: 0, y: 0, rotate: 0}
 
 document.addEventListener("DOMContentLoaded", ()=>{
     loadStage()
 })
 
 function loadStage(){
-    var grid = document.getElementById("grid")
+    const grid = document.getElementById("grid")
 
     plan.grid.forEach((piece, i)=>{
-        switch (piece) {
-            case "S":
-                player.x = i % plan.width
-                player.y = Math.floor(i / plan.width)
-                grid.innerHTML += `<div id="player"></div>`
-            case "#":
-                grid.innerHTML += `<div class="tile" style="--xpos: ${i % plan.width}; --ypos: ${Math.floor(i / plan.width)};"></div>`
-            break
+        let entity = plan.entities[piece]
+        if (entity) {
+            if (entity.exec) {entity.exec(i)}
+            let attributes = ''
+            attributes += entity.id ? `id="${entity.id}"`: ''
+            attributes += entity.class ? `class="${entity.class}"`: ''
+            var element = `<div ${attributes}>${entity.content || ''}</div>`
         }
+        grid.innerHTML += `<div class="tile ${entity?.tileclass || ''}" style="--xpos: ${i % plan.width}; --ypos: ${Math.floor(i / plan.width)};">${element || ""}</div>`
     })
     step()
 }
@@ -24,9 +24,9 @@ function loadStage(){
 window.addEventListener("keydown", step)
 
 function step(event = undefined) {
-    var forward = 0
-    var side = 0
-    var turn = 0
+    let forward = 0
+    let side = 0
+    let turn = 0
     switch (event?.key.toLowerCase()) {
         case "arrowup":
         case "w":
@@ -52,26 +52,37 @@ function step(event = undefined) {
         break
     }
     player.rotate += turn
+    let to_x = player.x
+    let to_y = player.y
     switch ((player.rotate % 360 + 360) % 360) {
         case 0:
-            player.x += side
-            player.y += forward
+            to_x += side
+            to_y += forward
         break
         case 90:
-            player.x -= forward
-            player.y += side
+            to_x -= forward
+            to_y += side
         break
         case 180:
-            player.x -= side
-            player.y -= forward
+            to_x -= side
+            to_y -= forward
         break
         case 270:
-            player.x += forward
-            player.y -= side
+            to_x += forward
+            to_y -= side
         break
+    }
+    if (document.querySelector(`.tile[style*="--xpos: ${to_x}; --ypos: ${to_y};"]`) && !document.querySelector(`.tile[style*="--xpos: ${to_x}; --ypos: ${to_y};"]`).classList.contains('nowalk')){
+        player.x = to_x
+        player.y = to_y
     }
     grid.style.setProperty('--playerx', player.x)
     grid.style.setProperty('--playery', player.y)
     grid.style.setProperty('--playerrotate', player.rotate)
     document.querySelector(`.tile[style*="--xpos: ${player.x}; --ypos: ${player.y};"]`).appendChild(document.getElementById('player'))
+    document.querySelectorAll(`.tile`).forEach(tile=>{
+        let diff = Math.abs(tile.style.getPropertyValue('--xpos') - player.x) + Math.abs(tile.style.getPropertyValue('--ypos') - player.y);
+        let opacity = 1.5 - diff * 0.25;
+        tile.style.setProperty('--bullshitopacity', opacity)
+    })
 }
